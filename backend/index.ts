f6a9e9b4e0fe6299d971277b603ef629d16bd6e2
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-11-15 23:36:46 +0100

    add prisma integration and seeding

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(cors());
 app.get("/", async (_, res) => res.json("Hello world"));
+
+app.get("/products", async (_, res) => {
+  const products = await prisma.product.findMany();
+  console.log(products);
+  return res.json(products);
+});
+
+app.get("/categories", async (_, res) => {
+  const categories = await prisma.category.findMany();
+  console.log(categories);
+  return res.json(categories);
+});
 
 app.listen(8000, () => console.log("App started on port 8000"));
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,44 @@
+import { Available, PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+const createRandomProduct = () => {
+  return {
+    name: faker.commerce.productName(),
+    categoryId: Math.floor(Math.random() * 3) + 1,
+    brand: faker.commerce.productAdjective(),
+    price: parseInt(faker.commerce.price(100, 30000, 0)),
+    available: faker.helpers.arrayElement([
+      Available.IN_STORE,
+      Available.WEB_ONLY,
+      Available.UNAVAILABLE,
+    ]),
+    discount: faker.helpers.maybe(() => "20%", { probability: 0.1 }) ?? null,
+    type: faker.commerce.department(),
+    view: parseInt(faker.random.numeric(2)),
+    promoted: faker.helpers.maybe(() => true, { probability: 0.1 }) ?? false,
+    type_info: faker.datatype.json(),
+  };
+};
+
+async function main() {
+  await prisma.$queryRaw`TRUNCATE TABLE Category`;
+  await prisma.$queryRaw`TRUNCATE TABLE Product`;
+
+  await prisma.category.createMany({
+    data: [
+      { name: "Motherboard" },
+      { name: "CPU" },
+      { name: "GPU" },
+      { name: "PSU" },
+      { name: "Hard Drive" },
+    ],
+  });
+
+  await prisma.product.createMany({
+    data: new Array(50).fill(0).map(() => createRandomProduct()),
+  });
+}
+
+main();
