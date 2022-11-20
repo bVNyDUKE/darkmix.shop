1462274b0dd180ee2f342b447278849c9d8ad6f6
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-11-20 16:41:03 +0100

    add error handling and search

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+require("express-async-errors");
+
 import cors from "cors";
 import prisma from "./db";
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(cors());
+
 app.get("/", async (_, res) => res.json("Hello world"));
 
 app.get("/products", async (_, res) => {
@@ -22,9 +26,31 @@ app.get("/products/:id", async (req, res) => {
   return res.json(product);
 });
 
+app.get("/search", async (req, res) => {
+  if (!req.query.name) {
+    return res.json({});
+  }
+
+  const result = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: req.query.name.toString(),
+      },
+    },
+    take: 10,
+    orderBy: {
+      view: "desc",
+    },
+  });
+
+  res.json(result);
+});
+
 app.get("/categories", async (_, res) => {
   const categories = await prisma.category.findMany();
   return res.json(categories);
 });
+
+app.use(errorHandler);
 
 export { app };
