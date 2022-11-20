844ec10878b1465edb29bdec231a02f9f6cd3dfc
mihailo <mihailostefanovic89@gmail.com>
2022-11-20 21:04:18 +0100

    remove type any

--- frontend/components/Shop/ShopCategory/ShopCategories.tsx
+++ frontend/components/Shop/ShopCategory/ShopCategories.tsx
@@ -9,7 +9,7 @@ export default function ShopCategories({categories} : {categories: Category[]}){
           </h2>
           <div className="sm:grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4">
             {categories.map((cat) => (
-              <ShopCategory props={cat} key={cat.id}/>
+              <ShopCategory cat={cat} key={cat.id}/>
             ))}
           </div>
         </div>
